17f6b61b4e65493c01b17f0ab8139a1e886f3bd6
nhducminh <35518770+nhducminh@users.noreply.github.com>
2024-09-04 14:07:41 +0700

    Update nhdminh_ASM7.py

--- nhdminh_ASM7.py
+++ nhdminh_ASM7.py
@@ -24,10 +24,14 @@ def kMeansImg(URL,k):
     return Image.fromarray(img_new)
 
 # img_url = "https://www.popsci.com/uploads/2023/05/15/ButterflyFamilyTree.png"
+col1, col2 = st.columns(2)
 k = st.slider("Select a range of k", 3, 16)
 img_url = st.text_input("Image URL", "https://www.popsci.com/uploads/2023/05/15/ButterflyFamilyTree.png")
+img_org = Image.open(requests.get(img_url, stream=True).raw)
+
+col1.image(img_org,use_column_width=True)
 if st.button(f'kMean URL {k}'):
     img_show = kMeansImg(img_url,k)
-    img_show
+    col2.image(img_show,,use_column_width=True)
 
 
