7c180c7187891d056125e5353de86e343eadecdb
nhducminh <35518770+nhducminh@users.noreply.github.com>
2024-09-04 13:46:46 +0700

    Add files via upload

---
+++ nhdminh_ASM7.py
@@ -0,0 +1,34 @@
+
+import requests
+from sklearn.cluster import KMeans
+import numpy as np
+from PIL import Image
+import streamlit as st
+
+
+def loadImg(URL):
+    from PIL import Image
+    img_url = URL
+    img_pil = Image.open(requests.get(img_url, stream=True).raw)
+    img_pil # ảnh lưu ở format Pillow
+
+    return np.array(img_pil) # ảnh lưu ở dạng numpy array
+    
+def kMeansImg(URL,k):
+    img = loadImg(URL)
+    X = img.reshape(img.shape[0]*img.shape[1],img.shape[2])
+    X.shape
+    kmeans = KMeans(n_clusters=k, n_init='auto')
+    kmeans.fit(X)
+    img_new= ([kmeans.cluster_centers_[kmeans.labels_[i]] for i in range(len(X))])
+    img_new= np.array(img_new).reshape(img.shape)
+    img_new = img_new.astype(np.uint8)    
+    return Image.fromarray(img_new)
+
+img_url = "https://www.popsci.com/uploads/2023/05/15/ButterflyFamilyTree.png"
+k=8
+
+k = st.slider("Select a range of SD", 3, 11)
+
+img_show = kMeansImg(img_url,k)
+img_show
