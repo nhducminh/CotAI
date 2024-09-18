7784debcbe2a5f37e2b2df29d36756b5115a3a9f
nhducminh <35518770+nhducminh@users.noreply.github.com>
2024-09-18 10:55:20 +0700

    Update nhdminh_ASM8.py

--- nhdminh_ASM8.py
+++ nhdminh_ASM8.py
@@ -22,6 +22,11 @@ with col1:
                 with col3:
                     st.dataframe(df)
                 with col4:
+                    option = st.selectbox(
+                        "How would you like to be contacted?",
+                        (df.columns[0], df.columns[1], df.columns[2]),
+                    )
+
                     pass
                 pass
             with tab2:
