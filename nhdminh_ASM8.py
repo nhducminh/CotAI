9c80dcaad08ae43aa2654d1cea9ba9d63906ab05
nhducminh <35518770+nhducminh@users.noreply.github.com>
2024-09-18 11:09:37 +0700

    Update nhdminh_ASM8.py

--- nhdminh_ASM8.py
+++ nhdminh_ASM8.py
@@ -33,13 +33,16 @@ with col1:
                     
                     # YOUR CODE HERE
                     y = df.Sales
+                    st.write(y)
                     model = LinearRegression()
 
                     if len(options)==1:                        
                         X = np.array(df.loc[:,options[0]])
+                        st.write(X)
                         # model.fit(X,y)
                     elif len(options)==2:
                         X = np.array(df.loc[:,[options[0],options[1]]])
+                        st.write(X)
                         # model.fit(X,y)
 
                     pass
