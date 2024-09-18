d1d8cee230c4b2dd70e9f4b41838b248aeef0e09
nhducminh <35518770+nhducminh@users.noreply.github.com>
2024-09-18 16:05:54 +0700

    Update nhdminh_ASM8.py

--- nhdminh_ASM8.py
+++ nhdminh_ASM8.py
@@ -56,8 +56,8 @@ with col1_main:
                             fig, ax = plt.subplots()
                             ax.scatter(X, y)
                             ax.plot(X,X*w+b,color='red')
-                            fig.xlabel(options[0])
-                            fig.ylabel('Sales')
+                            ax.set_xlabel(options[0])
+                            ax.set_ylabel('Sales')
                             st.pyplot(fig)
                         if len(options)==2:
                             X_space = np.linspace(np.min(X[:,0]),np.max(X[:,0]))
