7db9c803b4fcf77c1b3fc9290f7c7e3af99213bc
nhducminh <35518770+nhducminh@users.noreply.github.com>
2024-09-18 10:32:59 +0700

    Update nhdminh_ASM8.py

--- nhdminh_ASM8.py
+++ nhdminh_ASM8.py
@@ -22,10 +22,10 @@ with col2:
   # col2.header('TV')
   inputTV = st.number_input("Insert TV number")
 
-# if st.button("Predict", type="primary"):
-#   if inputRadio*inputTV!=0:
-#     y_input = np.array([[inputTV,inputRadio]])
-#     y_input_predict = model.predict(y_input)
-#     txt = st.text_area(f'Sale prediction: {y_input_predict}')
-#   else:
-#     txt = st.text_area(f'Please input TV and Radio buget')
+if st.button("Predict", type="primary"):
+  if inputRadio*inputTV!=0:
+    y_input = np.array([[inputTV,inputRadio]])
+    y_input_predict = model.predict(y_input)
+    txt = st.text_area(f'Sale prediction: {y_input_predict}')
+  else:
+    txt = st.text_area(f'Please input TV and Radio buget')
